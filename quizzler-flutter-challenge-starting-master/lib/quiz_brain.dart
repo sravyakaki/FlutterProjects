5fadd040d5cd8d22dee5a7ffe612432166817fc7
Sravya Kaki <co-skaki@cgsinc.ca>
2022-07-18 22:18:02 -0700

    some challenge

--- quizzler-flutter-challenge-starting-master/lib/quiz_brain.dart
+++ quizzler-flutter-challenge-starting-master/lib/quiz_brain.dart
@@ -4,31 +4,50 @@ class QuizBrain {
   int _questionNumber = 0;
 
   List<Question> _questionBank = [
-    Question('Some cats are actually allergic to humans', true),
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true),
-    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
-    Question('It is illegal to pee in the Ocean in Portugal.', true),
     Question(
-        'No piece of square dry paper can be folded in half more than 7 times.',
-        false),
+        questionText: 'Some cats are actually allergic to humans',
+        questionAnswer: true),
     Question(
-        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
-        true),
+        questionText: 'You can lead a cow down stairs but not up stairs.',
+        questionAnswer: false),
     Question(
-        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
-        false),
+        questionText:
+            'Approximately one quarter of human bones are in the feet.',
+        questionAnswer: true),
+    Question(questionText: 'A slug\'s blood is green.', questionAnswer: true),
     Question(
-        'The total surface area of two human lungs is approximately 70 square metres.',
-        true),
-    Question('Google was originally called \"Backrub\".', true),
+        questionText: 'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".',
+        questionAnswer: true),
     Question(
-        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
-        true),
+        questionText: 'It is illegal to pee in the Ocean in Portugal.',
+        questionAnswer: true),
     Question(
-        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
-        true),
+        questionText:
+            'No piece of square dry paper can be folded in half more than 7 times.',
+        questionAnswer: false),
+    Question(
+        questionText:
+            'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        questionAnswer: true),
+    Question(
+        questionText:
+            'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        questionAnswer: false),
+    Question(
+        questionText:
+            'The total surface area of two human lungs is approximately 70 square metres.',
+        questionAnswer: true),
+    Question(
+        questionText: 'Google was originally called \"Backrub\".',
+        questionAnswer: true),
+    Question(
+        questionText:
+            'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        questionAnswer: true),
+    Question(
+        questionText:
+            'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        questionAnswer: true),
   ];
 
   void nextQuestion() {
